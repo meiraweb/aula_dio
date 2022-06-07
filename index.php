3ebb8f7523a3508b9087c5932edfd8e2b74fb66c
Adilson <69163829+meiraweb@users.noreply.github.com>
2022-06-07 09:36:33 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html >
+<html>
+    <head>
+        <title>
+            Aula Dio - PHP
+        </title>
+        <meta content="charset utf-8" charset="UTF-8" />
+        <meta name="viewport" content="viewport=width-device=width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    </head>
+    <body>
+<?php
+
+
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
+
+</body>
+</html>
